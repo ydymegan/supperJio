e249fc0ea8002e2d3b99ef4462e0d4b4de8b8565
ydymegan <meganyee1404@gmail.com>
2021-05-27 19:25:35 +0800

    setup pages

---
+++ src/components/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from "react"
+import NavBar from '../layout/NavBar.js'
+
+export default function AboutUs() {
+    return (
+        <div>
+        <NavBar></NavBar>
+            This is the About Us Page
+        </div>
+    );
+}
---
+++ src/components/pages/ContactUs.js
@@ -0,0 +1,11 @@
+import React from "react"
+import NavBar from '../layout/NavBar.js'
+
+export default function ContactUs() {
+    return (
+        <div>
+        <NavBar></NavBar>
+            This is the Contact Us page
+        </div>
+    );
+}
