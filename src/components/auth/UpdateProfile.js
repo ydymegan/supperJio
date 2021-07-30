37d59b1671820de53d0a1a6930d220196f589f5e
ydymegan <meganyee1404@gmail.com>
2021-07-30 20:19:54 +0800

    final fixes

--- src/components/auth/UpdateProfile.js
+++ src/components/auth/UpdateProfile.js
@@ -1,58 +1,19 @@
-import React, { useRef, useState, useEffect } from "react"
+import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert, Container } from "react-bootstrap"
 import { useAuth } from "../../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
 import { db } from '../../firebase.js'
 
 export default function UpdateProfile() {
+  
+  const userRef = db.collection("users");
   const emailRef = useRef()
-  const usernameRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
-  const userRef = db.collection("users");
   const { currentUser, updatePassword, updateEmail } = useAuth()
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
-  const [username, setUsername] = useState("");
-  const [usernameList, setUsernameList] = useState([]);
   const history = useHistory()
-
-  var docRef = userRef.doc(currentUser.email);
-  docRef.get().then((doc) => {
-    setUsername(doc.data().username);
-  });
-
-  function getUsername() {
-    setLoading(true);
-    userRef.get().then(queryResult => {
-      const items = [];
-      queryResult.forEach(doc => {
-        const userDetails = doc.data();
-        items.push(userDetails.username);
-      });
-
-      setUsernameList(items);
-      setLoading(false);
-    });
-
-  }
-
-  useEffect(() => {
-    getUsername();
-    // eslint-disable-next-line
-  }, []);
-
-  function checkConflictingUsername(username) {
-    var i;
-
-    for (i = 0; i < usernameList.length; i++) {
-      if (usernameRef.current.value === usernameList[i]) {
-        return true;
-      }
-    }
-
-    return false;
-  }
 
   function handleSubmit(e) {
     e.preventDefault()
@@ -60,43 +21,26 @@ export default function UpdateProfile() {
       return setError("Passwords do not match")
     }
 
-    if (checkConflictingUsername(username)) {
-      return setError("Username is Taken")
-    }
-
     const promises = []
     setLoading(true)
     setError("")
 
-    if (usernameRef.current.value && usernameRef.current.value !== userRef.doc(currentUser.email)) {
+    if (emailRef.current.value !== currentUser.email) {
+      promises.push(updateEmail(emailRef.current.value));
       userRef.doc(currentUser.email).update({
-        username: usernameRef.current.value,
+        email: emailRef.current.value
       })
     }
     if (passwordRef.current.value) {
-      userRef.doc(currentUser.email).update({
-        password: passwordRef.current.value,
-      })
       promises.push(updatePassword(passwordRef.current.value))
-    }
-    if (emailRef.current.value !== currentUser.email) {
       userRef.doc(currentUser.email).update({
-        email: emailRef.current.value,
+        password: passwordRef.current.value
       })
-      userRef.doc(currentUser.email).get().then(function (doc) {
-        if (doc && doc.exists) {
-          var data = doc.data();
-          userRef.doc(emailRef.current.value).set(data);
-          userRef.doc(currentUser.email).delete();
-        }
-      });
-      promises.push(updateEmail(emailRef.current.value))
     }
-
 
     Promise.all(promises)
       .then(() => {
-        history.push("/login")
+        history.push("/")
       })
       .catch(() => {
         setError("Failed to update account")
@@ -107,6 +51,7 @@ export default function UpdateProfile() {
   }
 
   return (
+    <>
     <Container
       className="d-flex align-items-center justify-content-center"
       style={{ minHeight: "100vh" }}>
@@ -125,21 +70,12 @@ export default function UpdateProfile() {
                   defaultValue={currentUser.email}
                 />
               </Form.Group>
-              <Form.Group id="username">
-                <Form.Label>Username</Form.Label>
-                <Form.Control
-                  type="username"
-                  ref={usernameRef}
-                  required
-                  defaultValue={username}
-                />
-              </Form.Group>
               <Form.Group id="password">
                 <Form.Label>Password</Form.Label>
                 <Form.Control
                   type="password"
                   ref={passwordRef}
-                  placeholder="Leave blank to keep the same"
+                  placeholder="Insert Your New Password Here"
                 />
               </Form.Group>
               <Form.Group id="password-confirm">
@@ -147,7 +83,7 @@ export default function UpdateProfile() {
                 <Form.Control
                   type="password"
                   ref={passwordConfirmRef}
-                  placeholder="Leave blank to keep the same"
+                  placeholder="Insert Your New Password Here"
                 />
               </Form.Group>
               <Button disabled={loading} className="w-100" type="submit">
@@ -160,6 +96,8 @@ export default function UpdateProfile() {
           <Link to="/">Cancel</Link>
         </div>
       </div>
-    </Container>
+      </Container>
+    </>
   )
 }
+
